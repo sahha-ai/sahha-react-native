ebda9233f0190c0f02c01df51c3f48f209e3e1d9
smileypop <matthewlaird@gmail.com>
2022-02-03 15:57:19 -0700

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# sahha-react-native
+
+Integrate Sahha sdk your React Native app
+
+## Installation
+
+```sh
+npm install sahha-react-native
+```
+
+## Usage
+
+```js
+import { multiply } from "sahha-react-native";
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
---
+++ android/src/main/java/com/sahhareactnative/SahhaReactNativeModule.kt
@@ -0,0 +1,24 @@
+package com.sahhareactnative
+
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.Promise
+
+class SahhaReactNativeModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
+
+    override fun getName(): String {
+        return "SahhaReactNative"
+    }
+
+    // Example method
+    // See https://reactnative.dev/docs/native-modules-android
+    @ReactMethod
+    fun multiply(a: Int, b: Int, promise: Promise) {
+    
+      promise.resolve(a * b)
+    
+    }
+
+    
+}
